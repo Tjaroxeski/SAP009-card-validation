16395257d69600d606479d92e353c64dd8319d94
TataSchultz <tarcila.schultz@gmail.com>
2023-01-16 15:01:38 -0300

    Primeira versão- estrutura html e primeiros stilos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import validator from './validator.js';
 
 console.log(validator);
+//inserir as interaçoes do script com o html (realizar a manipulacao do DOM aqui)
+
+
--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,5 @@ const validator = {
 };
 
 export default validator;
+
+//inserir a funçao para validar o cartão(aqui é a logica do JS)
