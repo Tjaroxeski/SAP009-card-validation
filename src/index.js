4a9d8731df4f32027c32ebeffd1e80bf3ba3b0f6
Tharla <jaroxeski2021@gmail.com>
2023-01-31 21:32:03 -0300

    cardvalidation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+
+const inputNumeros = document.getElementById ("cardNumber") 
+const button = document.getElementById   ("button")
+const mensagem = document.getElementById ("mensagem")
+
+button.addEventListener("click",verificado);
+
+
+function verificado (e){
+
+  e.preventDefault();
+  const  digito = inputNumeros.value
+  const validar = validator.isValid(digito);
+
+    
+  if (
+    digito.trim() === "" ||    
+      digito === "0000000000000000" ||
+      digito === "000000000000000" ||
+      digito === "00000000000000" ||
+      digito.length <= 13
+  ) {
+    return (mensagem.innerHTML = "Por favor, digite o número do cartão!");
+  }  
+  else if (validar === true) {
+    return (mensagem.innerHTML = "Cartão válido!");
+  }
+  else if (!validar) {  
+    return (mensagem.innerHTML = "Cartão inválido. Digite novamente!");
+  } 
+
+  else if (validator === true) {
+  return (messagem.innerHTML=`Cartão válido ${validator.maskify(digito)} `)
+
+  }
+ 
+  else if (!validator === true)  {
+ return(mensagem.innerHTML = 'Cartão inválido')
+
+}
+
+
