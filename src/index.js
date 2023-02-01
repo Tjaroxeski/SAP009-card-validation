dfafb9563f2b93960cf19c41f99a2079dcf05e94
TataSchultz <tarcila.schultz@gmail.com>
2023-02-01 12:17:34 -0300

    projeto com validator.js funcionando

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
-import {isValid} from './validator.js'
+import { isValid, maskify } from './validator.js'
 
 
 
 const creditCardNumber = document.getElementById("cc"); // chamar id ou classe do html
-const buttonSend= document.getElementById("enviar");// chamar o botão do html
+//const buttonSend = document.getElementById("enviar");// chamar o botão do html
 //const messageConfirm= document.getElementById("mensagem");//fazer aparecer mensagem de confirmaçao ou erro 
+const formulario = document.getElementById("formulario");// chamar o botão do html
 
 
 
-buttonSend.addEventListener("click",  cardValidation); // adicionar evento no botao vai chamar funçao card validation 
+formulario.addEventListener("submit", cardValidation); // adicionar evento no botao vai chamar funçao card validation 
 // criar funcao com as regras de digitacao para o card validation
 
-function cardValidation(){
+function cardValidation() {
    //console.log("teste")
-  //evento.preventDefault() // evento para pagina nao atualizar depois que a pagina atualizar 
-    let custumerInput = creditCardNumber.value;
-    //console.log(custumerInput)
+   //evento.preventDefault() // evento para pagina nao atualizar depois que a pagina atualizar 
+   const custumerInput = creditCardNumber.value;
+   //console.log(custumerInput)
    //let validateCard = validator.isValid(custumerInput);
-    //regras para aceitacao 
-    if(
-       custumerInput==="" || 
-       custumerInput==="0000000000000000" ||
-       custumerInput==="000000000000000" ||
-       custumerInput==="00000000000000" ||
-       custumerInput.length <=13
+   //regras para aceitacao 
+   if (
+      custumerInput === "" ||
+      custumerInput === "0000000000000000" ||
+      custumerInput === "000000000000000" ||
+      custumerInput === "00000000000000" ||
+      custumerInput.length <= 13
 
-      ){
-       alert("Dados incompletos, por favor verifique os números do cartão e digite novamente!");
-    }
+   ) {
+      alert("Dados incompletos, por favor verifique os números do cartão e digite novamente!");
+   }
 
-    else if(isValid(custumerInput)===true){
-       return alert ("O cartão foi validado, em breve você receberá seu pedido!");
-    }else { 
-       (isValid(custumerInput) ===false)
-       alert("Ops! Algo de errado aconteceu, por favor verifique os dados do cartão e tente novamente.")
-       }
-      }
-    
+   else if (isValid(custumerInput) === true) {
+      alert("O cartão foi validado, em breve você receberá seu pedido!");
+   } else {
+      (isValid(custumerInput) === false)
+      alert("Ops! Algo de errado aconteceu, por favor verifique os dados do cartão e tente novamente.")
+   }
+   creditCardNumber.value = maskify(custumerInput)
+}
--- src/validator.js
+++ src/validator.js
@@ -1,73 +1,42 @@
-export function 
-isValid(creditCardNumber) { 
+export function
+  isValid(creditCardNumber) {
 
- const arrayCreditCardNumber= creditCardNumber.split("").reverse(); //esse número vai gerar um array com 16 números e reverter
+  const arrayCreditCardNumber = creditCardNumber.split("").reverse(); //esse número vai gerar um array com 16 números e reverter
   //console.log("hello", arrayCreditCardNumber);//ok- ta funfando!
   let totalSum = 0
-  
-    
-    
-         for (let i = 0; i <= arrayCreditCardNumber.length -1; i++){
-           //console.log(arrayCreditCardNumber); //ok, funfando a array invertida
-           let digit = parseInt(arrayCreditCardNumber[i]);
-            if ( i % 2 === 0) {
-                digit *= 2;
-                if (digit > 9){
-                  digit -=9;
-                } 
-                
-            }
 
-            totalSum += digit;
-         }
-        if ( totalSum % 10 === 0){
-          return true 
-        }
-        return false
 
-        }
-        
-    
 
-        //     if ((i +1) % 2 ===0){
-        //         arrayCreditCardNumber[i]= (Number(arrayCreditCardNumber[i]))*2
-               
-        //         if(Number(arrayCreditCardNumber[i] >= 10)){
-        //             (arrayCreditCardNumber[i])= (Number(arrayCreditCardNumber[i]-9))
-                   
-                   
-        //             totalSum = totalSum + Number(arrayCreditCardNumber[i])
-               
-               
-        //          } else {
-        //             totalSum = totalSum + Number(arrayCreditCardNumber[i])};
-        //             console.log(totalSum);
-        //     }
-        //     if (totalSum % 10 === 0){
-        //         return true
-               
-        //     } else {
-        //         return false
-        //     }
-        //     }
-        // }
-    
-    
-         
-    
-    
-        //  maskify: (custumerInput) => {
-        //     if (custumerInput.length <= 4){
-        //         return custumerInput
-        //     }
-           
-        //     const ultimosDigitos= custumerInput.slice(-4);
-        //     const simboloMascara = "#";
-        //     const qtdCaracteres = custumerInput.length -4;
-        //     const numeroMascarado= simboloMascara.repeat(qtdCaracteres)+ ultimosDigitos;
-        //     return numeroMascarado
-        //  }
-       
+  for (let i = 0; i <= arrayCreditCardNumber.length - 1; i++) {
+    //console.log(arrayCreditCardNumber); //ok, funfando a array invertida
+    let digit = parseInt(arrayCreditCardNumber[i]);
+    if (i % 2 === 0) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+
+    }
+
+    totalSum += digit;
+  }
+  if (totalSum % 10 === 0) {
+    return true
+  }
+  return false
+
+}
+
+export function maskify(custumerInput) {
+  if (custumerInput.length <= 4) {
+    return custumerInput
+  }
+
+  const ultimosDigitos = custumerInput.slice(-4);
+  const simboloMascara = "#";
+  const qtdCaracteres = custumerInput.length - 4;
+  const numeroMascarado = simboloMascara.repeat(qtdCaracteres) + ultimosDigitos;
+  return numeroMascarado
+}
+
        //export default validator;
-        // const pairPosition= arrayCreditCardNumber.length
-    
