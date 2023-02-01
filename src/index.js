8510638b924c55d5187f89c75d53aa7042c0b359
Tharla <jaroxeski2021@gmail.com>
2023-02-01 16:59:09 -0300

    ajuste indentação

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
 import validator from "./validator.js";
 
 
-const inputNumeros = document.getElementById ("cardNumber") 
-const button = document.getElementById   ("button")
-const mensagem = document.getElementById ("mensagem")
+const inputNumeros = document.getElementById("cardNumber")
+const button = document.getElementById("button")
+const mensagem = document.getElementById("mensagem")
 
-button.addEventListener("click",verificado);
+button.addEventListener("click", verificado);
 
 
-function verificado (e){
+function verificado(e) {
+
+
 
   e.preventDefault();
-  const  digito = inputNumeros.value
+  const digito = inputNumeros.value
   const validar = validator.isValid(digito);
 
-    
+
   if (
-    digito.trim() === "" ||    
-      digito === "0000000000000000" ||
-      digito === "000000000000000" ||
-      digito === "00000000000000" ||
-      digito.length <= 13
+    digito.trim() === "" ||
+    digito === "0000000000000000" ||
+    digito === "000000000000000" ||
+    digito === "00000000000000" ||
+    digito.length <= 13
   ) {
+
     return (mensagem.innerHTML = "Por favor, digite o número do cartão!");
-  }  
-  else if (validar === true) {
-    return (mensagem.innerHTML = "Cartão válido!");
   }
-  else if (!validar) {  
+  else if (validar === true) {
+    return mensagem.innerHTML = ("Cartão válido!");
+  }
+  else if (!validar) {
     return (mensagem.innerHTML = "Cartão inválido. Digite novamente!");
-  } 
+  }
 
   else if (validator === true) {
-  return (messagem.innerHTML=`Cartão válido ${validator.maskify(digito)} `)
+    return (mensagem.innerHTML = `Cartão válido ${validator.maskify(digito)} `)
 
   }
- 
-  else if (!validator === true)  {
- return(mensagem.innerHTML = 'Cartão inválido')
 
+  else if (!validator === true) {
+    return (mensagem.innerHTML = 'Cartão inválido')
+
+  }
 }
 
 
